f118ac5a7526a895e63837b1003a8f6b03f458ca
Max M <46329167+max01110@users.noreply.github.com>
2023-04-04 15:41:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ import time
 import analogio
 import pwmio
 from adafruit_motor import servo
+import adafruit_rgbled
 
-# Configure the internal GPIO connected to the LED as a digital output
-led_green = digitalio.DigitalInOut(board.GP16)
-led_green.direction = digitalio.Direction.OUTPUT
 
-led_blue = digitalio.DigitalInOut(board.GP17)
-led_blue.direction = digitalio.Direction.OUTPUT
+led_uv = digitalio.DigitalInOut(board.GP21)
+led_uv.direction = digitalio.Direction.OUTPUT
 
-led_red = digitalio.DigitalInOut(board.GP18)
-led_red.direction = digitalio.Direction.OUTPUT
+
+led_blue = board.GP20
+
+
+led_green = board.GP17
+
+led_red = board.GP16
+
+led = adafruit_rgbled.RGBLED(led_red, led_blue, led_green)
+
 
 fans1 = digitalio.DigitalInOut(board.GP1)
 fans1.direction = digitalio.Direction.OUTPUT
+
 
 #fans.value = False
 
@@ -88,13 +95,16 @@ def adc_to_voltage(adc_value):
 
 #######LOW LEVEL FUNCTIONS############
 def buzz():
-    global buzzer
+    global buzzer, led
     for duty in range(5):
         # increasing duty cycle
         buzzer.duty_cycle = 10000
+        led.color = (255, 0, 0)
         time.sleep(0.2)
+        led.color = (0, 0, 0)
         buzzer.duty_cycle = 0
         time.sleep(0.2)
+
 
 
 
@@ -104,33 +114,35 @@ def coolingOn():
     fans1.value = True
 
     print("Cooling On")
-    
+
     if openValve == False:
         openValve = True
+        my_servo.angle = 90
         timePrev = time.time()
-        for i in range(11):
+        for i in range(8):
             my_servo.angle = 1
             time.sleep(0.001)
 
-    
+
 
     my_servo.angle = 90
 
     #Turn on fans
 
-   
+
 
 
 
 def coolingOff():
-    global fans1
+    global fans1, openValve
     fans1.value = False
+    openValve = False
 
     print("Cooling Stopped")
    # time.sleep(1)
 
 def doorOpen():
-    global ranOnceClosed, ranOnceOpen, led_red, led_green, led_blue, doorOpenThreshold, timePrev
+    global ranOnceClosed, ranOnceOpen, led, led_red, led_green, led_blue, led_uv, doorOpenThreshold, timePrev
     if ranOnceOpen == 0:
         ranOnceOpen = 1
         ranOnceClosed = 0
@@ -142,41 +154,41 @@ def doorOpen():
         buzz()
         timePrev = time.time()
 
-
-    led_red.value = True
-    led_green.value = True
-    led_blue.value = False
+    led.color = (0, 0, 0)
+    led_uv.value = False
 
 
 def doorClosed():
-    global ranOnceClosed, ranOnceOpen, interval, led_red, led_green, led_blue, UVTimeON, sleepTime, closedDoorState, timePrev
+    global ranOnceClosed, ranOnceOpen, interval, led, led_red, led_green, led_blue, led_uv, UVTimeON, sleepTime, closedDoorState, timePrev
+    led.color = (255, 255, 255)
+
     if ranOnceClosed == 0:
         ranOnceClosed = 1
         ranOnceOpen = 0
         print("Main Lights: OFF")
         timePrev = time.time()
-        led_green.value = False #TURN OFF MAIN LIGHTS
-        led_red.value = False #TURN OFF MAIN LIGHTS
+        led.color = (0, 0, 0)
+
         closedDoorState = 1
 
     #LIGHTING (UV light)
     if closedDoorState == 0:
         if (time.time() - timePrev < sleepTime):
             print("UV Lights: ON")
-            led_blue.value = True
+            led_uv.value = True
 
 
 
         else:
             timePrev = time.time()
             closedDoorState = 1
-            
+
 
     else:
         if (time.time() - timePrev < UVTimeON):
             print("UV Lights: OFF")
-            led_blue.value = False
-          
+            led_uv.value = False
+
         else:
 
             timePrev = time.time()
@@ -190,18 +202,18 @@ valveClosed = 1
 
 # Loop so the code runs continuously
 while True:
-    
+
     if (time.time() - timePrevValve > valveOpenThreshold):
         if valveClosed == 0:
             valveClosed = 1
-            for i in range(11):
+            for i in range(7):
                 my_servo.angle = 1
                 time.sleep(0.001)
-    
-            my_servo.angle = 90
-            
 
-    
+            my_servo.angle = 90
+
+
+
     if pressed == False:
         if button.value == False: # If the button is pressed
             if coolOn == False:
@@ -210,8 +222,8 @@ while True:
                 valveClosed = 0
             else:
                 coolOn = False
-        
-   
+
+
             pressed = True
     elif pressed == True: # If
         if button.value == True: # If the button is depressed
@@ -225,8 +237,9 @@ while True:
         lightVal = (adc_to_voltage(photoresistor.value))
 
     time.sleep(0.1)
+    print(lightVal)
 
-    if lightVal > 62000:
+    if lightVal > 55000:
         door = 0
     else:
         door = 1
@@ -235,7 +248,7 @@ while True:
     #Water cooling system
     if coolOn == True:
         coolingOn()
-        
+
     else:
         coolingOff()
 
@@ -244,8 +257,8 @@ while True:
     if door == 0:
 
         doorClosed()
-        
-      
+
+
 
     else:
         doorOpen()
